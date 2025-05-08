f9103bdf01aa409100b6afd92ed2e74442725d02
jinzcdev <jinzcdev@gmail.com>
2025-05-08 16:38:20 +0800

    feat(playground): support loading .xmind files and parsing to XMindMark format

--- playground/src/loader.ts
+++ playground/src/loader.ts
@@ -1,10 +1,25 @@
+import { parseXMindToXMindMarkFile } from '../../src'
+
 export async function loadFileAsText(file: File): Promise<string> {
   return new Promise((resolve, reject) => {
     const fileReader = new FileReader()
-    fileReader.addEventListener('loadend', () => resolve(fileReader.result as string))
     fileReader.addEventListener('error', reject)
     fileReader.addEventListener('abort', reject)
-    fileReader.readAsText(file)
+    const suffix = file.name.split('.').slice(-1)[0]
+    if (suffix === 'xmind') {
+      fileReader.readAsArrayBuffer(file)
+      fileReader.addEventListener('loadend', async () => {
+        const content = await parseXMindToXMindMarkFile(fileReader.result as ArrayBuffer);
+        if (content) {
+          resolve(content)
+        } else {
+          reject(new Error('Not valid .xmind file.'))
+        }
+      })
+    } else {
+      fileReader.readAsText(file)
+      fileReader.addEventListener('loadend', () => resolve(fileReader.result as string))
+    }
   })
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { parseXMindMarkToXMindFile } from './lib/xmindmark-to-xmind'
+export { parseXMindToXMindMarkFile } from './lib/xmind-to-xmindmark'
